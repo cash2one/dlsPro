334a1b63a345e58729d924f3a1963de06db48f42
spf <2629960826@qq.com>
2014-11-05 20:36:48 +0800

    将base中的heu....js调换到check_right
    s

--- static/script/checkup.js
+++ static/script/checkup.js
@@ -17,3 +17,40 @@ function checkpage(page)
 	var zhi = document.getElementsByName("search_value")[0].value;
 	location.href = "/t/checkup"+page+"&value="+value+"&zhi="+zhi;
 }
+$(document).ready(function ()
+{
+ $("#infolistbg :radio").change(function ()
+{               
+    var value=$(this).val();
+    var xmlhttp;
+    var json;
+    if (window.XMLHttpRequest)
+    {// code for IE7+, Firefox, Chrome, Opera, Safari
+        xmlhttp=new XMLHttpRequest();
+    }
+    else
+     {// code for IE6, IE5
+         xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xmlhttp.onreadystatechange=function()
+    {
+    if (xmlhttp.readyState==4 && xmlhttp.status==200)
+    {
+         json=xmlhttp.responseText;
+         // var json_obj = json.parseJSON();
+         // var json_obj = JSON.parse(json);
+         var str = json.split(",");
+         // alert(str[0]);
+         var bianhao = document.getElementsByName("bianhao");
+         for(var i=0;i<bianhao.length;i++)
+            {
+                bianhao[i].innerHTML = str[i];
+                // alert(str[i]);
+         
+            }
+    }
+  }
+    xmlhttp.open("GET","/t/check_eq?eq_id="+value,true);
+    xmlhttp.send();       
+});
+});       
--- static/script/geo.js
+++ static/script/geo.js
@@ -17,46 +17,6 @@ function export_xls()
 {
     location.href = "/t/export_xls";
 }
-
- $(document).ready(function ()
-{
- $("#infolistbg :radio").change(function ()
-{               
-    var value=$(this).val();
-    var xmlhttp;
-    var json;
-    if (window.XMLHttpRequest)
-    {// code for IE7+, Firefox, Chrome, Opera, Safari
-        xmlhttp=new XMLHttpRequest();
-    }
-    else
-     {// code for IE6, IE5
-         xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-    }
-    xmlhttp.onreadystatechange=function()
-    {
-    if (xmlhttp.readyState==4 && xmlhttp.status==200)
-    {
-         json=xmlhttp.responseText;
-         // var json_obj = json.parseJSON();
-         // var json_obj = JSON.parse(json);
-         var str = json.split(",");
-         // alert(str[0]);
-         var bianhao = document.getElementsByName("bianhao");
-         for(var i=0;i<bianhao.length;i++)
-            {
-                bianhao[i].innerHTML = str[i];
-                // alert(str[i]);
-         
-            }
-    }
-  }
-    xmlhttp.open("GET","/t/check_eq?eq_id="+value,true);
-    xmlhttp.send();       
-});
-});       
-
-
 function delete_build()
 {
    alert("确定删除？")
