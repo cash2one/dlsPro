323a6ee10e8526c63d4c3e3e67de87e1e9f384d0
zhihui <zhihui20080313@126.com>
2015-02-03 21:54:40 +0800

    intro
    点击蒙版不关闭提示

--- static/script/intro.js
+++ static/script/intro.js
@@ -47,7 +47,7 @@
       /* Close introduction when pressing Escape button? */
       exitOnEsc: true,
       /* Close introduction when clicking on overlay layer? */
-      exitOnOverlayClick: true,
+      exitOnOverlayClick: false,
       /* Show step numbers in introduction? */
       showStepNumbers: true,
       /* Let user use keyboard to navigate the tour? */
